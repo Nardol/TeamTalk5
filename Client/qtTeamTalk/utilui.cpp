afc97377f9b87b4622f4708214574c6103174659
Patrick ZAJDA <patrick@zajda.fr>
2025-12-14 18:33:46 +0100

    qtTeamTalk: avoid blocking DBus check and guard queued notify

--- Client/qtTeamTalk/utilui.cpp
+++ Client/qtTeamTalk/utilui.cpp
@@ -71,14 +71,7 @@ bool linuxNotificationsServiceAvailable()
         return false;
 
     QCoreApplication* app = QCoreApplication::instance();
-    if (app && QThread::currentThread() != app->thread())
-    {
-        bool result = false;
-        QMetaObject::invokeMethod(app, [&result]() {
-            result = linuxNotificationsServiceAvailableImpl();
-        }, Qt::BlockingQueuedConnection);
-        return result;
-    }
+    Q_UNUSED(app);
 
     return linuxNotificationsServiceAvailableImpl();
 }
@@ -934,6 +927,8 @@ void showNotification(const QString& title, const QString& message)
         const QString titleCopy = title;
         const QString messageCopy = message;
         QMetaObject::invokeMethod(app, [titleCopy, messageCopy]() {
+            if (QCoreApplication::closingDown())
+                return;
             showNotificationImpl(titleCopy, messageCopy);
         }, Qt::QueuedConnection);
         return;
